6fc59e4f22432db10511f519f4f0a7a4c0bed9cd
rodergstep <rodergstep@gmail.com>
2025-07-11 09:26:06 +0300

    added nodeSelector, vars, updated pools

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,34 @@ variable "worker_node_pools" {
     location       = string
   }))
 }
+
+variable "hcloud_token" {
+  description = "Hetzner Cloud API token."
+  type        = string
+  sensitive   = true
+}
+
+variable "master_server_type" {
+  description = "The server type for the master node."
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy the cluster in."
+  type        = string
+}
+
+variable "ssh_key_name" {
+  description = "The name of the SSH key to use."
+  type        = string
+}
+
+variable "ssh_public_key_path" {
+  description = "The path to the public SSH key."
+  type        = string
+}
+
+variable "local_ip_for_ssh" {
+  description = "Your local IP address for SSH access."
+  type        = string
+}
